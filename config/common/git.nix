f90c0b9cfcbd4ccb46c28bb06234c4a84419c181
fym998 <61316972+fym998@users.noreply.github.com>
2025-11-28 21:06:14 +0800

    `programs.git.settings.init.defaultBranch = "main"`

--- config/common/git.nix
+++ config/common/git.nix
@@ -6,6 +6,7 @@
         Name = "fym998";
         email = "61316972+fym998@users.noreply.github.com";
       };
+      init.defaultBranch = "main";
       # https://forums.whonix.org/t/git-users-enable-fsck-by-default-for-better-security/2066
       transfer.fsckobjects = true;
       fetch.fsckobjects = true;
