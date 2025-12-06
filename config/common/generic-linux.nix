f6b9b6bb802cd8fd2235999802e0688ccb07bd15
fym998 <61316972+fym998@users.noreply.github.com>
2025-12-06 21:38:01 +0800

    add `lib.genericLinux.nullIfEnabled`

--- config/common/generic-linux.nix
+++ config/common/generic-linux.nix
@@ -6,14 +6,17 @@
 }:
 {
   targets.genericLinux.enable = true;
-  lib.genericLinux.wrapIfEnabled =
-    pkg: cmd:
-    if !config.targets.genericLinux.enable then
-      pkg
-    else
-      pkgs.writeShellScriptBin "${builtins.head (lib.splitString " " cmd)}-nix-wrapper" ''
-        exec /usr/bin/${cmd} "$@"
-      '';
-  lib.genericLinux.getCmd =
-    pkg: cmd: if !config.targets.genericLinux.enable then "${pkg}/bin/${cmd}" else "/usr/bin/${cmd}";
+  lib.genericLinux = {
+    wrapIfEnabled =
+      pkg: cmd:
+      if !config.targets.genericLinux.enable then
+        pkg
+      else
+        pkgs.writeShellScriptBin "${builtins.head (lib.splitString " " cmd)}-nix-wrapper" ''
+          exec /usr/bin/${cmd} "$@"
+        '';
+    nullIfEnable = pkg: if !config.targets.genericLinux.enable then pkg else null;
+    getCmd =
+      pkg: cmd: if !config.targets.genericLinux.enable then "${pkg}/bin/${cmd}" else "/usr/bin/${cmd}";
+  };
 }
